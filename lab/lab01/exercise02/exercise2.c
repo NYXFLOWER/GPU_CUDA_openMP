ab2206139328f6d241520812b959c0c7fcfd9de7
nyxflower <hxu31@sheffield.ac.uk>
2019-02-15 11:19:55 +0000

    update ex02

---
+++ lab/lab01/exercise02/exercise2.c
@@ -0,0 +1,39 @@
+//
+// Created by Hao Xu on 2019-02-14.
+//
+#include "random.h"
+#include <stdio.h>
+
+#define NUM_VALUES 250
+
+long long int values[NUM_VALUES];
+
+int main() {
+    unsigned long long int sum = 0;
+    long long int average = 0;
+    unsigned char i = 0;
+    int temp = 0, max = 0, min = 0;
+
+    init_random();
+
+    for (i = 0; i < NUM_VALUES; ++i) {
+        values[i] = random_uint();
+//        printf("i: %d, value[%d]: %d\n", i, i, value[i]);
+        sum += values[i];
+    }
+
+    average = sum / NUM_VALUES;
+
+    for (i = 0; i < NUM_VALUES; ++i) {
+        values[i] -= average;
+        min = (values[i] < min) ? (int) values[i] : min;
+        max = (values[i] > max) ? (int) values[i] : max;
+    }
+
+    printf("Sum = %llu\n", sum);
+    printf("Average = %lli\n", average);
+    printf("Min = %d\n", min);
+    printf("Max = %d\n", max);
+};
+
+
---
+++ lab/lab01/exercise02/random.c
@@ -0,0 +1,20 @@
+//
+// Created by Hao Xu on 2019-02-15.
+//
+#include "random.h"
+
+unsigned int rseed;
+
+void init_random(){
+    srand(rseed);
+}
+
+unsigned short random_ushort(){
+    return rand();
+}
+
+unsigned int random_uint() {
+    rseed = RANDOM_A * rseed + RANDOM_C;
+
+    return rseed;
+}
